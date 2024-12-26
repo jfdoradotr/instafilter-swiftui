7e3057dbc70d7915b44e50d080ae39047bea2867
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 15:00:38 -0600

    Small example of `ContentUnavailableView`

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -7,32 +7,8 @@ import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-  @State private var image: Image?
-
   var body: some View {
-    VStack {
-      image?
-        .resizable()
-        .scaledToFit()
-    }
-    .onAppear(perform: loadImage)
-  }
-
-  private func loadImage() {
-    let inputImage = UIImage(resource: .example)
-    let beginImage = CIImage(image: inputImage)
-
-    let context = CIContext()
-    let currentFilter = CIFilter.sepiaTone()
-
-    currentFilter.inputImage = beginImage
-    currentFilter.intensity = 1
-
-    guard let outputImage = currentFilter.outputImage else { return }
-    guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-
-    let uiImage = UIImage(cgImage: cgImage)
-    image = Image(uiImage: uiImage)
+    ContentUnavailableView("No snippets available", systemImage: "swift")
   }
 }
 
