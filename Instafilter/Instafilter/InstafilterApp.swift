d885f7bf106c8c407c4c35f22934fdc79f66e935
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 12:57:10 -0600

    Initial project

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Juan Francisco Dorado Torres on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
