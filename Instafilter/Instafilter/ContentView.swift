b5996c75ee5743ddd0af8d4436ce2a44dfb285b6
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 17:29:53 -0600

    Import PhotosUI so we can use the photos Picker to import an image

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -2,26 +2,30 @@
 // Copyright © Juan Francisco Dorado Torres. All rights reserved.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
   @State private var processedImage: Image?
   @State private var filterIntensity = 0.5
+  @State private var selectedItem: PhotosPickerItem?
 
   var body: some View {
     NavigationStack {
       VStack {
         Spacer()
-        if let processedImage {
-          processedImage
-            .resizable()
-            .scaledToFit()
-        } else {
-          ContentUnavailableView(
-            "No picture",
-            systemImage: "photo.badge.plus",
-            description: Text("Tap to import a photo")
-          )
+        PhotosPicker(selection: $selectedItem) {
+          if let processedImage {
+            processedImage
+              .resizable()
+              .scaledToFit()
+          } else {
+            ContentUnavailableView(
+              "No picture",
+              systemImage: "photo.badge.plus",
+              description: Text("Tap to import a photo")
+            )
+          }
         }
         Spacer()
         HStack {
