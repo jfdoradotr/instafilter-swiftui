e220a574b27eeb7dc5f08d0da2be8b6363ad5abd
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 17:23:56 -0600

    Move to empty state

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -2,15 +2,12 @@
 // Copyright © Juan Francisco Dorado Torres. All rights reserved.
 //
 
-import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-  @Environment(\.requestReview) var requestReview
-  
   var body: some View {
-    Button("Leave a review") {
-      requestReview()
+    VStack {
+      
     }
   }
 }
