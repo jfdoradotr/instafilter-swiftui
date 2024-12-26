33dbb226dff3739069cfdb9788adc0df7fab01b4
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 17:28:46 -0600

    Wrap into a NavigationStack

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -9,32 +9,34 @@ struct ContentView: View {
   @State private var filterIntensity = 0.5
 
   var body: some View {
-    VStack {
-      Spacer()
-      if let processedImage {
-        processedImage
-          .resizable()
-          .scaledToFit()
-      } else {
-        ContentUnavailableView(
-          "No picture",
-          systemImage: "photo.badge.plus",
-          description: Text("Tap to import a photo")
-        )
-      }
-      Spacer()
-      HStack {
-        Text("Intensity")
-        Slider(value: $filterIntensity)
-      }
-      HStack {
-        Button("Change Filter", action: changeFilter)
+    NavigationStack {
+      VStack {
         Spacer()
-        // share the picture
+        if let processedImage {
+          processedImage
+            .resizable()
+            .scaledToFit()
+        } else {
+          ContentUnavailableView(
+            "No picture",
+            systemImage: "photo.badge.plus",
+            description: Text("Tap to import a photo")
+          )
+        }
+        Spacer()
+        HStack {
+          Text("Intensity")
+          Slider(value: $filterIntensity)
+        }
+        HStack {
+          Button("Change Filter", action: changeFilter)
+          Spacer()
+          // share the picture
+        }
       }
+      .padding([.horizontal, .bottom])
+      .navigationTitle("Instafilter")
     }
-    .padding([.horizontal, .bottom])
-    .navigationTitle("Instafilter")
   }
 
   private func changeFilter() {
