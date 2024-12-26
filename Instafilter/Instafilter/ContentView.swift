5f6da916d159ec69bf5f6d108e948296fd5cd351
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 15:03:00 -0600

    Example using actions

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,7 +8,16 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    ContentUnavailableView("No snippets available", systemImage: "swift", description: Text("You don't have any saved snippets yet"))
+    ContentUnavailableView {
+      Label("No snippets", systemImage: "swift")
+    } description: {
+      Text("You don't have any saved snippets yet.")
+    } actions: {
+      Button("Create snippet") {
+
+      }
+      .buttonStyle(.borderedProminent)
+    }
   }
 }
 
