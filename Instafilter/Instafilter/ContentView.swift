d7d010beab2053111dc833e2877f5d8107574d0b
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 15:01:30 -0600

    Using `description`

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,7 +8,7 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    ContentUnavailableView("No snippets available", systemImage: "swift")
+    ContentUnavailableView("No snippets available", systemImage: "swift", description: Text("You don't have any saved snippets yet"))
   }
 }
 
