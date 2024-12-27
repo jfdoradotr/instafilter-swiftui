0aaba3250470bbb8122a1f01adcffe785b735f52
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 20:03:30 -0600

    Fix to set a value according to the possible input key the filter contains

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -78,7 +78,11 @@ struct ContentView: View {
   }
 
   private func applyProcessing() {
-    currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+    let inputKeys = currentFilter.inputKeys
+
+    if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+    if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+    if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
 
     guard let outputImage = currentFilter.outputImage else { return }
     guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
